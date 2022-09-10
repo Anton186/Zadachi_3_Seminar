b3862d6dff9c30a170c1133da5e694a350407131
Чернышев Антон <black2604@mail.ru>
2022-09-10 16:08:54 +0500

    задача 19 выполнена

--- zadacha19/Program.cs
+++ zadacha19/Program.cs
@@ -12,7 +12,25 @@ if (number < -9999 && number > -100000)
 }
 if (number > 9999 && number < 100000)
 {
-    
+    int number1 = number / 10000;
+    int number5 = number %10;
+    if (number1 == number5)
+    {
+        int number2 = number / 1000 %10;
+        int number4 = number %100 /10;
+        if (number2 == number4)
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом");
+        }
+    }
+    else
+        {
+            Console.WriteLine("Число не является палиндромом");
+        }
 }
 else
 {
